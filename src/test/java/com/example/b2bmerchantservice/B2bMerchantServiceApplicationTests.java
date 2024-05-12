254fe66659a319b922402b5c1b494906d51a2065
Muhammadqodir Berdiyev <m.berdiev@uzum.com>
2024-05-12 22:51:25 +0500

    project initialization

---
+++ src/test/java/com/example/b2bmerchantservice/B2bMerchantServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.b2bmerchantservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B2bMerchantServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
